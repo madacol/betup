a9163319651c4e71cf47c7837da9817951ee1579
Marco D'Agostini <madacol10@gmail.com>
2020-12-04 22:03:23 -0500

    Deploy to vercel

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,3 @@
-<script>
-	import successkid from 'images/successkid.jpg';
-</script>
-
 <style>
 	h1, figure, p {
 		text-align: center;
@@ -41,10 +37,5 @@
 </svelte:head>
 
 <h1>Great success!</h1>
-
-<figure>
-	<img alt="Success Kid" src="{successkid}">
-	<figcaption>Have fun with Sapper!</figcaption>
-</figure>
 
 <p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
